47d68745707189d4a25213c05b44dc98fda6bf2f
Satya Kalyan <satyakalyan5@gmail.com>
2023-10-26 21:19:35 -0600

    Moving on from actuators

---
+++ springboot/src/main/java/com/example/springboot/demo/myapp/rest/FunRestControlller.java
@@ -0,0 +1,26 @@
+package com.example.springboot.demo.myapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestControlller {
+
+    //expose a '/' endpoint returning "Hello World!"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Worldddd!";
+    }
+
+    //expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    //expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
+}
